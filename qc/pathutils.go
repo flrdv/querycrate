1800beef49005b26b4de085e57cb91e10091c738
fakefloordiv <real.floordiv@gmail.com>
2022-02-15 16:35:56 +0200

    added some new funcs to reduce code duplicating, but some are just useful

--- qc/pathutils.go
+++ qc/pathutils.go
@@ -2,14 +2,11 @@ package qc
 
 import (
 	"io/ioutil"
+	"os"
 	"path"
 	"path/filepath"
 	"strings"
 )
-
-func joinUnix(paths ...string) string {
-	return strings.Join(paths, "/")
-}
 
 /*
 	Pretty naive function
@@ -21,7 +18,7 @@ func makePathUnixLike(path string) string {
 func getFilteredFiles(dir Dir, filters ...Filter) (files []File) {
 	for _, filter := range filters {
 		for _, file := range dir.Files {
-			if filter.IsAcceptable(file) {
+			if filter.IsAllowed(file) {
 				files = append(files, file)
 			}
 		}
@@ -37,7 +34,7 @@ func getFilteredFiles(dir Dir, filters ...Filter) (files []File) {
 /*
 	Builds a tree of files, where returned Dir is a headPath
 */
-func buildFilesTree(rootPath string) (dir Dir, err error) {
+func buildFilesTree(rootPath string, maxRecursionDepth int) (dir Dir, err error) {
 	dir.Path = rootPath
 	files, err := ioutil.ReadDir(rootPath)
 
@@ -49,8 +46,12 @@ func buildFilesTree(rootPath string) (dir Dir, err error) {
 		fname := f.Name()
 
 		if f.IsDir() {
+			if maxRecursionDepth <= 0 {
+				continue
+			}
+
 			var newDir Dir
-			newDir, err = buildFilesTree(path.Join(rootPath, fname))
+			newDir, err = buildFilesTree(path.Join(rootPath, fname), maxRecursionDepth-1)
 
 			if err != nil {
 				return dir, err
@@ -58,25 +59,44 @@ func buildFilesTree(rootPath string) (dir Dir, err error) {
 
 			dir.Dirs = append(dir.Dirs, newDir)
 		} else {
-			dir.Files = append(dir.Files, File{
-				RelPath: rootPath,
-				Name:    fname,
-				// Be careful: extension always starts with a dot
-				Extension: filepath.Ext(fname),
-			})
+			dir.Files = append(dir.Files, newFile(rootPath, fname))
 		}
 	}
 
 	return dir, nil
 }
 
+func newFile(path, name string) File {
+	ext := filepath.Ext(name)
+
+	return File{
+		RelPath:   path,
+		Name:      removeExt(name, ext),
+		Extension: ext,
+	}
+}
+
+func getFile(path string) (string, string, error) {
+	content, err := os.ReadFile(path)
+
+	if err != nil {
+		return "", "", err
+	}
+
+	return removeExt(makePathUnixLike(path), filepath.Ext(path)), string(content), nil
+}
+
 /*
 	Just makes sure path is unix-like and doesn't have an extension
 */
 func getQueryPath(file File) string {
-	return removeExt(makePathUnixLike(path.Join(file.RelPath, file.Name)), file.Extension)
+	return removeHeadingFolder(makePathUnixLike(path.Join(file.RelPath, file.Name)))
 }
 
 func removeExt(path, ext string) string {
 	return path[:len(path)-len(ext)]
+}
+
+func removeHeadingFolder(path string) string {
+	return path[strings.Index(path, "/")+1:]
 }
